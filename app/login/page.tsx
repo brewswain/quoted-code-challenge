9e6eb593fd718a54c323fdf2aa66461cf64dae14
Brandon Lee <brandonlee2543@hotmail.com>
2023-06-26 14:14:05 -0400

    Code Cleanup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,5 @@
 import React from "react";
-import { firebaseAuth } from "../firebase";
 import EmailAuthentication from "../components/authentication/EmailAuthentication";
-
-let loading = false;
-
-const signIn = async () => {
-  loading = true;
-
-  try {
-  } catch (error) {}
-};
-
-const signUp = async () => {};
 
 const LoginPage = () => {
   return (
