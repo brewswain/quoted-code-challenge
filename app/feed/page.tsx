1d994a6d5ef8aac16955df3a727369293a3ff21c
Brandon Lee <brandonlee2543@hotmail.com>
2023-06-27 19:01:22 -0400

    Fixed some styling

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -9,35 +9,53 @@ import { firebaseAuth } from "../firebase";
 import { firebaseSignOut } from "../firebase/authentication";
 import { getUserFromDB } from "../firebase/firestore/getUser";
 
+import AddCommentIcon from "@mui/icons-material/AddComment";
+import { IconButton } from "@mui/material";
+
 const FeedPage = () => {
   const [isNewAccount, setIsNewAccount] = useState<boolean | null>(null);
 
-  const router = useRouter();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(
-      firebaseAuth,
-      async (currentUser) => {
-        if (currentUser) {
+  const getCurrentUser = onAuthStateChanged(
+    firebaseAuth,
+    async (currentUser) => {
+      if (currentUser) {
+        try {
           const user = await getUserFromDB(currentUser.uid);
-
-          if (user) {
-            if (user.user_name) {
-              return null;
-            } else {
-            }
-          }
+          return user;
+        } catch (error) {
+          console.error(error);
         }
+      } else {
+        console.log("user not detected");
       }
-    );
-    // Cleanup the event listener on component unmount
-    return () => unsubscribe();
-  }, []);
+    }
+  );
+
+  getCurrentUser();
 
   return (
-    <div>
+    <div className="h-screen">
       Feed Page
       <button onClick={() => firebaseSignOut()}>sign out</button>
+      <IconButton
+        size="large"
+        sx={{
+          position: "absolute",
+          bottom: "6rem",
+          right: "2rem",
+          // Prevent hover state from removing background color
+          "&:hover": {
+            bgcolor: "rgb(var(--icon-button-rgb))",
+          },
+        }}
+        // For some reason, backgroundColor/bgColor while NOT hovered is the only property on sx that reverts once the page loads. everything else works,
+        // and it's minor enough that I don't want to waste time delving into this, so I'll just put the style directly here
+        style={{
+          backgroundColor: "rgb(var(--icon-button-rgb))",
+        }}
+      >
+        <AddCommentIcon fontSize="inherit" className="text-white" />
+      </IconButton>
     </div>
   );
 };
