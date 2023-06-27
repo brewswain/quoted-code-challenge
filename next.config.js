7dcb45a72d43ad52ec0cea4b669e44175ed45508
Brandon Lee <brandonlee2543@hotmail.com>
2023-06-27 19:02:09 -0400

    Allowed firebase's CDN to be used for image hosting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "firebasestorage.googleapis.com",
+        protocol: "https",
+        port: "",
+        
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
