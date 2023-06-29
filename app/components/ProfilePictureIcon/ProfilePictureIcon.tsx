ff269cc594db8bd32bb62c351226ce99c9a75d5c
Brandon Lee <brandonlee2543@hotmail.com>
2023-06-29 14:53:56 -0400

    Manually adjusted dimension values

--- app/components/ProfilePictureIcon/ProfilePictureIcon.tsx
+++ app/components/ProfilePictureIcon/ProfilePictureIcon.tsx
@@ -3,16 +3,23 @@ import Image from "next/image";
 interface ProfilePictureIconProps {
   imageUrl: string;
   name: string;
+  dimensions?: number;
 }
 
-const ProfilePictureIcon = ({ imageUrl, name }: ProfilePictureIconProps) => {
+const ProfilePictureIcon = ({
+  imageUrl,
+  name,
+  dimensions,
+}: ProfilePictureIconProps) => {
   return (
+    // Hacky take on customizing dimensions, but depending on aspect ratio, even with width
+    // and height explicitly set in the props, it would be warped.
     <Image
       alt={`User ${name}'s Profile picture`}
       src={imageUrl}
-      width={50}
-      height={50}
-      className="rounded-full"
+      width={dimensions ? dimensions : 50}
+      height={dimensions ? dimensions : 50}
+      className={`rounded-full h-[${dimensions}px] w-[${dimensions}px] `}
     />
   );
 };
