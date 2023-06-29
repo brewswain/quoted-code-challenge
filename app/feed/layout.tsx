a8a01af5561fb32f2b601e03e4bf8cbde9d13a43
Brandon Lee <brandonlee2543@hotmail.com>
2023-06-29 14:54:14 -0400

    formatting change

--- app/feed/layout.tsx
+++ app/feed/layout.tsx
@@ -2,12 +2,7 @@ import React from "react";
 import NavBar from "../components/Authentication/NavBar";
 
 const FeedLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="feed">
-      {children}
-      <NavBar />
-    </div>
-  );
+  return <div className="feed">{children}</div>;
 };
 
 export default FeedLayout;
