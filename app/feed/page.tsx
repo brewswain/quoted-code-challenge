b032871a4a41cca69fab550b46dbea255fcc1ffa
Brandon Lee <brandonlee2543@hotmail.com>
2023-06-28 20:31:27 -0400

    Neatened up our Feed Page and began  implementing Real time UI updates WIP

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,61 +1,29 @@
-"use client";
+import NewQuoteButton from "../components/Quotes/NewQuoteButton";
+import {
+  getAllQuotes,
+  getQuotesSubscription,
+} from "../firebase/firestore/quotes/getQuotes";
+import QuoteCard from "../components/Quotes/QuoteCard";
 
-import { useState, useEffect } from "react";
+export const revalidate = 60;
 
-import { useRouter } from "next/navigation";
+const FeedPage = async () => {
+  const response = await getAllQuotes();
+  const quotes = await getQuotesSubscription();
 
-import { onAuthStateChanged } from "firebase/auth";
-import { firebaseAuth } from "../firebase";
-import { firebaseSignOut } from "../firebase/authentication";
-import { getUserFromDB } from "../firebase/firestore/getUser";
+  console.log({ quotes });
 
-import AddCommentIcon from "@mui/icons-material/AddComment";
-import { IconButton } from "@mui/material";
-
-const FeedPage = () => {
-  const [isNewAccount, setIsNewAccount] = useState<boolean | null>(null);
-
-  const getCurrentUser = onAuthStateChanged(
-    firebaseAuth,
-    async (currentUser) => {
-      if (currentUser) {
-        try {
-          const user = await getUserFromDB(currentUser.uid);
-          return user;
-        } catch (error) {
-          console.error(error);
-        }
-      } else {
-        console.log("user not detected");
-      }
-    }
-  );
-
-  getCurrentUser();
-
+  quotes.map((quote) => console.log(quote));
   return (
-    <div className="h-screen">
-      Feed Page
-      <button onClick={() => firebaseSignOut()}>sign out</button>
-      <IconButton
-        size="large"
-        sx={{
-          position: "absolute",
-          bottom: "6rem",
-          right: "2rem",
-          // Prevent hover state from removing background color
-          "&:hover": {
-            bgcolor: "rgb(var(--icon-button-rgb))",
-          },
-        }}
-        // For some reason, backgroundColor/bgColor while NOT hovered is the only property on sx that reverts once the page loads. everything else works,
-        // and it's minor enough that I don't want to waste time delving into this, so I'll just put the style directly here
-        style={{
-          backgroundColor: "rgb(var(--icon-button-rgb))",
-        }}
-      >
-        <AddCommentIcon fontSize="inherit" className="text-white" />
-      </IconButton>
+    <div className="h-4 flex flex-col">
+      <div className="pb-20">
+        {quotes
+          ? quotes.map((quote) => (
+              <QuoteCard quoteParam={quote} key={quote.uid} />
+            ))
+          : null}
+      </div>
+      <NewQuoteButton />
     </div>
   );
 };
