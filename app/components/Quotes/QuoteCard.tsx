defb66ba74a11bd3d8f8fd52326a1842a1296bd7
Brandon Lee <brandonlee2543@hotmail.com>
2023-06-29 14:54:06 -0400

    Began fleshing out QUoteCard design

--- app/components/Quotes/QuoteCard.tsx
+++ app/components/Quotes/QuoteCard.tsx
@@ -5,8 +5,9 @@ import { useState, useEffect } from "react";
 import { Quote } from "@/app/firebase/firestore/quotes/getQuotes";
 
 import moment from "moment";
-import { onAuthStateChanged } from "firebase/auth";
-import { firebaseAuth } from "@/app/firebase";
+
+import ProfilePictureIcon from "../ProfilePictureIcon/ProfilePictureIcon";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 interface QuoteCardProps {
   quoteParam: Quote;
@@ -14,15 +15,13 @@ interface QuoteCardProps {
 const QuoteCard = ({ quoteParam }: QuoteCardProps) => {
   const [timeStamp, setTimeStamp] = useState<string>("");
   const [user, setUser] = useState();
-  const { quote, user_name, likes, created_at } = quoteParam;
+  const { quote, user_name, likes, created_at, profile_picture } = quoteParam;
 
   // cloning our Date to prevent accidental modification of source
   const calculateTimestamps = () => {
     const createdAtWrapper = moment(created_at.seconds * 1000);
     const messageTimeInMinutes = createdAtWrapper.minute();
-    const formattedTimestamp = createdAtWrapper.format(
-      "ddd, MMM Do YYYY - h:mm A"
-    );
+    const formattedTimestamp = createdAtWrapper.format("MMM Do ");
     const currentTimeInMinutes = moment().minute();
     const timeSinceLastMessage = currentTimeInMinutes - messageTimeInMinutes;
 
@@ -50,12 +49,40 @@ const QuoteCard = ({ quoteParam }: QuoteCardProps) => {
   }, [timeStamp]);
 
   return (
-    <div className="flex flex-col border-b border-[#6a6a6a] p-4 mt-[calc(50% - 50vh)] mb-[calc(50% - 50vh)]">
-      <h2 className="item-center">{quote}</h2>
-      <p>- {user_name}</p>
-      <p>{timeStamp}</p>
-      {/* {user_name === } */}
-    </div>
+    <section className="flex flex-col border-b border-[#6a6a6a] p-4 mt-[calc(50% - 50vh)] mb-[calc(50% - 50vh)] min-h-[8rem]">
+      {/* Semantic HTML is used a bit more in this component than otherwise since there was initially a bunch of nested divs for layout */}
+      <div id="quote__layout-container" className="flex">
+        <aside
+          id="quote__picture-container"
+          className="flex justify-start items-start h-[35px]"
+        >
+          <ProfilePictureIcon
+            name={user_name}
+            imageUrl={profile_picture}
+            dimensions={35}
+          />
+        </aside>
+        <div className="flex flex-col pl-8 max-w-[90%]">
+          <div className="flex mb-2">
+            <p className="text-[0.9rem] text-zinc-400">{timeStamp}</p>
+          </div>
+          <article id="quote__text-container ">
+            <h1 className="item-center text-3xl">{`${quote}`}</h1>
+
+            <p className="pr-4 text-zinc-400 text-xl">- {user_name} </p>
+            {/* {user_name === } */}
+            {/*  */}
+          </article>
+          <footer
+            id="quote__footer"
+            className="mt-4 flex justify-start w-full pr-4"
+          >
+            <FavoriteBorderIcon className="cursor-pointer text-zinc-200 hover:text-[rgb(var(--icon-button-rgb))]" />
+            <span className="pl-2  text-zinc-400">{likes}</span>
+          </footer>
+        </div>
+      </div>
+    </section>
   );
 };
 
