35050399badfb2fe21432c51944cc9bd0142bac4
Brandon Lee <brandonlee2543@hotmail.com>
2023-06-29 20:32:18 -0400

    Added Navbar to feed layout specifically

--- app/feed/layout.tsx
+++ app/feed/layout.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import NavBar from "../components/Authentication/NavBar";
 
 const FeedLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="feed">{children}</div>;
+  return (
+    <div className="feed">
+      {children}
+      <NavBar />
+    </div>
+  );
 };
 
 export default FeedLayout;
