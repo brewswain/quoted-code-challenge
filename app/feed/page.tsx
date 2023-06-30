5ea97a033c66454c85ca25311033df6b5abd73bd
Brandon Lee <brandonlee2543@hotmail.com>
2023-06-29 23:09:50 -0400

    Added more relevant Quote Data to ouor QuoteCards

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -7,11 +7,18 @@ import {
   getQuotesSubscription,
 } from "../firebase/firestore/quotes/getQuotes";
 import QuoteCard from "../components/Quotes/QuoteCard";
+import { firebaseAuth } from "../firebase";
+import { getUserFromDB } from "../firebase/firestore/users/getUser";
+import { DocumentData } from "firebase/firestore";
+import { useRouter } from "next/navigation";
 
 export const revalidate = 60;
 
 const FeedPage = () => {
   const [quotes, setQuotes] = useState<Quote[]>([]);
+  const [uid, setUid] = useState<string>("");
+
+  const router = useRouter();
 
   useEffect(() => {
     // Tne callback here is responsible for updating our state by calling setQuotes with our
@@ -25,20 +32,40 @@ const FeedPage = () => {
 
     // Pretty neat! this is the first time i've used this implementation pattern, so I went a bit
     // deeper into this comment for my benefit in particular.
-    const unsubscribe = getQuotesSubscription((updatedQuotes) => {
+    const unsubscribeFromQuotes = getQuotesSubscription((updatedQuotes) => {
       setQuotes(updatedQuotes);
     });
 
+    const unsubscribeFromUser = firebaseAuth.onAuthStateChanged(
+      async (currentUser) => {
+        if (currentUser) {
+          setUid(currentUser.uid);
+        } else if (!currentUser) {
+          console.error("User not found");
+          router.push("/login");
+        }
+      }
+    );
+
     return () => {
-      unsubscribe();
+      unsubscribeFromQuotes();
+      unsubscribeFromUser();
     };
   }, []);
+
+  useEffect(() => {
+    console.log({ uid });
+  }, [uid]);
 
   return (
     <div className="h-4 flex flex-col">
       <div className="pb-20">
+        {/*  for like functionality we can make sure that whenever a user likes and unlikes a post, they 
+        update their profile in the database with an array of quote uids. we then pass this array as a prop 
+        into quotecard, where we check to see if the current post's UID matches up. if the post is liked? 
+        we adjust styling of our heart to represent that. */}
         {quotes.map((quote) => (
-          <QuoteCard quoteParam={quote} key={quote.uid} />
+          <QuoteCard quoteParam={quote} key={quote.uid} userUid={uid} />
         ))}
       </div>
       <NewQuoteButton />
