29c2b53f3fdfe9f4970c49c5657b711b5dd311bf
Brandon Lee <brandonlee2543@hotmail.com>
2023-06-30 16:43:26 -0400

    Created page to show our liked posts

---
+++ app/user/likes/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import {
+  Quote,
+  getQuotesSubscription,
+} from "@/app/firebase/firestore/quotes/getQuotes";
+import { useRouter } from "next/navigation";
+import QuoteCard from "@/app/components/Quotes/QuoteCard";
+import QuoteCardSkeleton from "@/app/components/Skeletons/QuoteCardSkeleton";
+import Image from "next/image";
+
+const LikesPage = () => {
+  const [likedQuotes, setLikedQuotes] = useState<Quote[]>([]);
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const searchParams = useSearchParams();
+
+  const userUid = searchParams.get("userUid");
+
+  const filterLikedQuotes = (userUid: string, quotes: Quote[]): Quote[] => {
+    // Filter the quotes array to find the ones that match the user's likedQuotes
+    return quotes.filter((quote: Quote) => {
+      return quote.likes.some((like) => like.userUid === userUid);
+    });
+  };
+
+  useEffect(() => {
+    setLoading(true);
+
+    const unsubscribeFromQuotes = getQuotesSubscription((updatedQuotes) => {
+      setQuotes(updatedQuotes);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribeFromQuotes();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (userUid && quotes.length > 0) {
+      const filteredLikedQuotes = filterLikedQuotes(userUid, quotes);
+      setLikedQuotes(filteredLikedQuotes);
+      console.log({ quotes, likedQuotes });
+    }
+  }, [userUid, quotes]);
+
+  return (
+    <div className="h-4 flex flex-col">
+      <div className="pb-20">
+        {loading ? (
+          Array.from({ length: 3 }).map((_, index) => (
+            <div className="pl-8" key={index}>
+              <QuoteCardSkeleton />
+            </div>
+          ))
+        ) : likedQuotes.length === 0 ? (
+          <div className="flex items-center h-[calc(100vh-5rem)] justify-center ">
+            <p className="w-9/12">
+              No liked quotes found :( Go back and like a couple quotes!
+            </p>
+            <Image
+              src="https://i.redd.it/urtqrwgb0qya1.jpg"
+              alt="A picture of kirby looking surprised"
+              height={150}
+              width={150}
+            />
+          </div>
+        ) : (
+          likedQuotes.map((quote) => (
+            <QuoteCard quoteParam={quote} key={quote.uid} userUid={userUid!} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default LikesPage;
