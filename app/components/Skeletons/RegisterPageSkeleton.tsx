e4226466439f4c821676255bfe8f56c3d22987a3
Brandon Lee <brandonlee2543@hotmail.com>
2023-07-01 14:54:11 -0400

    Update RegisterPageSkeleton.tsx

--- app/components/Skeletons/RegisterPageSkeleton.tsx
+++ app/components/Skeletons/RegisterPageSkeleton.tsx
@@ -1,15 +1,13 @@
 const RegisterPageSkeleton = () => {
   return (
-    <div className="flex flex-col items-center w-[90vw] m-0 m-auto mt-20">
-      <div className="animate-pulse">
-        <div className="w-40 h-6 mb-4 bg-gray-300 rounded"></div>
-        <div className="w-full h-6 mb-2 bg-gray-300 rounded"></div>
-        <div className="w-full h-6 mb-2 bg-gray-300 rounded"></div>
-        <div className="w-full h-6 mb-2 bg-gray-300 rounded"></div>
-        <div className="w-full h-6 mb-2 bg-gray-300 rounded"></div>
+    <div className="flex flex-col items-center w-[90vw] m-0 m-auto mt-24">
+      <div className="animate-pulse w-9/12">
+        <div className="w-full h-8 mb-12 bg-gray-300 rounded"></div>
       </div>
-      <div className="flex items-end justify-center py-4 w-9/12">
-        <div className="w-full h-12 bg-gray-300 rounded"></div>
+      <div className="flex items-end  flex-col justify-center py-4 w-9/12 animate-pulse">
+        <div className="w-full h-8 mb-6 bg-gray-300 rounded"></div>
+        <div className="w-full h-9 mb-6  bg-gray-300 rounded"></div>
+        <div className="w-full h-9 bg-gray-300 rounded"></div>
       </div>
     </div>
   );
